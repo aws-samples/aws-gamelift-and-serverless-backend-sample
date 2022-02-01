50fc3565c2e1917356089a5637d62be4999c9c95
juhoaws <jjantun@amazon.com>
2022-02-01 13:47:10 -0800

    Client build options to Unity menu + readme update

--- GameLiftExampleUnityProject/Assets/Editor/ClientServerConfiguration.cs
+++ GameLiftExampleUnityProject/Assets/Editor/ClientServerConfiguration.cs
@@ -37,4 +37,36 @@ public class ClientServerConfiguration : Editor
         // Build player
         BuildPipeline.BuildPlayer(levels, path + "/GameLiftExampleServer.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.EnableHeadlessMode);
     }
+
+    [MenuItem("GameLift/BuildMacOSClient")]
+    private static void BuildMacOSClient()
+    {
+        // Set scripting define symbols
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "CLIENT");
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, "CLIENT");
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, "CLIENT");
+
+        // Get filename
+        string path = EditorUtility.SaveFolderPanel("Choose Location of MacOS Build", "", "");
+        string[] levels = new string[] { "Assets/Scenes/GameWorld.unity" };
+
+        // Build player
+        BuildPipeline.BuildPlayer(levels, path + "/GameClient.app", BuildTarget.StandaloneOSX, BuildOptions.None);
+    }
+
+    [MenuItem("GameLift/BuildWindowsClient")]
+    private static void BuildWindowsClient()
+    {
+        // Set scripting define symbols
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "CLIENT");
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, "CLIENT");
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, "CLIENT");
+
+        // Get filename
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Windows Build", "", "");
+        string[] levels = new string[] { "Assets/Scenes/GameWorld.unity" };
+
+        // Build player
+        BuildPipeline.BuildPlayer(levels, path + "/GameClient.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
 }

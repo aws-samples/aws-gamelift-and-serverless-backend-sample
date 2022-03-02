e2ab93171d1937a874442a8f682c35945402241d
juhoaws <jjantun@amazon.com>
2022-03-02 15:53:39 -0800

    Check for tools installation before trying to deploy backend

--- deployBackendAndPreRequirements.sh
+++ deployBackendAndPreRequirements.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# 0. Check that we have the tools installed
+
+if ! [ -x "$(command -v aws)" ]; then
+  echo 'Error: AWS CLI is not installed'
+  exit 1
+fi
+
+if ! [ -x "$(command -v sam)" ]; then
+  echo 'Error: SAM is not installed'
+  exit 1
+fi
+
+if ! [ -x "$(command -v node)" ]; then
+  echo 'Error: Node is not installed'
+  exit 1
+fi
+
 echo "1. Deploy Serverless Backend"
 cd GameServiceAPI
 ./deploy.sh
